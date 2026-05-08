437f8831c84bdcf13978ab845d7f80a4673a69da
Jayaswuroopa <v.jayaswuroopa@gmail.com>
2026-05-08 12:28:12 +0530

    Initial commit with fixes for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/portfolio">
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <div className="relative">
           <Navigation />
           <Routes>
